ff90419d6ac99e55528de07e631da5ad96bdd49f
IgorCzudy <iczudy@gmail.com>
2025-01-29 13:39:21 +0100

    feat: security config

---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.security;
+
+
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private final UserService userService;
+
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity httpSecurity) throw Exception{
+        return httpSecurity.
+    }
+}
