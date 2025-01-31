8ced247f2f9a73103b180e9706c179d189638659
IgorCzudy <iczudy@gmail.com>
2025-01-31 13:57:28 +0100

    feat: security config

---
+++ src/main/java/com/example/demo/configuration/DataLoader.java
@@ -0,0 +1,22 @@
+package com.example.demo.security;
+
+import com.example.demo.model.Book;
+import com.example.demo.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataLoader {
+
+    @Bean
+    public CommandLineRunner loadData(BookRepository bookRepository) {
+        return args -> {
+            if (bookRepository.count() == 0) { // Only insert if empty
+                bookRepository.save(new Book("Adam Mickiewicz", "Pan Tadeusz", "/images/panTadeusz.jpg"));
+                bookRepository.save(new Book("Tolken", "Hobbit", "/images/hobbit.jpg"));
+                bookRepository.save(new Book("George Orwell", "1984", "/images/1984.jpeg"));
+            }
+        };
+    }
+}
--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -5,19 +5,45 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private final UserService userService;
+//    @Autowired
+//    private final UserService userService;
+
 
     @Bean
-    public SecurityFilterChain SecurityFilterChain(HttpSecurity httpSecurity) throw Exception{
-        return httpSecurity.
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/index.html", "/static/**", "/login", "/register").permitAll() // Allow access to home page and static resources
+                        .requestMatchers("/books/**rate").authenticated()
+                        .anyRequest().permitAll() // Allow other requests to be accessible without authentication
+                )
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?error=true")
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
     }
 }
